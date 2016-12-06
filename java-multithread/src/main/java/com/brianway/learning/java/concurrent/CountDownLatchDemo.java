b1bdbe144b9e08f46f7b31e7b5f3c810f39cf9d8
brianway <250902678@qq.com>
2016-12-06 20:15:37 +0800

    [add] add CountDownLatch example

---
+++ java-multithread/src/main/java/com/brianway/learning/java/concurrent/CountDownLatchDemo.java
@@ -0,0 +1,81 @@
+package com.brianway.learning.java.concurrent;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class CountDownLatchDemo {
+    static final int SIZE = 10;
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        // All must share a single CountDownLatch object:
+        CountDownLatch latch = new CountDownLatch(SIZE);
+        for (int i = 0; i < 10; i++)
+            exec.execute(new WaitingTask(latch));
+        for (int i = 0; i < SIZE; i++)
+            exec.execute(new TaskPortion(latch));
+        System.out.println("Launched all tasks");
+        exec.shutdown(); // Quit when all tasks complete
+    }
+}
+
+// Performs some portion of a task:
+class TaskPortion implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private static Random rand = new Random(47);
+    private final CountDownLatch latch;
+
+    TaskPortion(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void run() {
+        try {
+            doWork();
+            latch.countDown();
+            latch.await();
+            System.out.println(this + " TaskPortion after await");
+        } catch (InterruptedException ex) {
+            // Acceptable way to exit
+        }
+    }
+
+    public void doWork() throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(rand.nextInt(2000));
+        System.out.println(this + "completed");
+    }
+
+    public String toString() {
+        return String.format("%1$-3d ", id);
+    }
+}
+
+// Waits on the CountDownLatch:
+class WaitingTask implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private final CountDownLatch latch;
+
+    WaitingTask(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void run() {
+        try {
+            latch.await();
+            System.out.println("Latch barrier passed for " + this);
+        } catch (InterruptedException ex) {
+            System.out.println(this + " interrupted");
+        }
+    }
+
+    public String toString() {
+        return String.format("WaitingTask %1$-3d ", id);
+    }
+}
+
+/* (Execute to see output) *///:~

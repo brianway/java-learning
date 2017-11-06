526a85e2d5ee8c3348ee3002ceb21e5d31cafe93
caoqiwen2001 <727616488@qq.com>
2017-11-06 16:00:24 +0800

    本地测试java

--- java-base/src/main/java/com/brianway/learning/java/base/constructor/SonClass.java
+++ java-base/src/main/java/com/brianway/learning/java/base/constructor/SonClass.java
@@ -25,7 +25,7 @@ public class SonClass extends FatherClass {
     }
 
     public static void main(String[] args) {
-        new SonClass("aaa");
+       // new SonClass("aaa");
         new SonClass();
     }
 
